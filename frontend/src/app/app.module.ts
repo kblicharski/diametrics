b957c7ef6d41a408d071b9b31dba1613503f9a01
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-10-28 04:44:51 -0500

    New project styling is working now

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HeaderItemComponent } from './header/header-item/header-item.component';
+import { HeaderComponent } from '../../../website/src/app/header/header.component';
+import { HeaderItemComponent } from '../../../website/src/app/header/header-item/header-item.component';
 import { PageNotFoundComponent } from '../../../website/src/app/utility/page-not-found/page-not-found.component';
 import { AboutComponent } from './pages/about/about.component';
 import { AppRoutingModule } from '../../../website/src/app/app-routing.module';
--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './pages/about/about.component';
 import { PageNotFoundComponent } from './utility/page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderItemComponent } from './header/header-item/header-item.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    HeaderItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
