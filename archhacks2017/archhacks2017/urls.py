61c74e8287122149bb7493f718ad83fdd108c6b1
Madeline Silva <mad.silva.ic@gmail.com>
2017-10-27 23:34:24 -0500

    Initial model definitions.

--- archhacks2017/archhacks2017/urls.py
+++ archhacks2017/archhacks2017/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^', include('diabetes_stats.urls')),
 ]
--- archhacks2017/diabetes_stats/models.py
+++ archhacks2017/diabetes_stats/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class BloodGlucoseEvent(models.Model):
+    time = models.DateTimeField(auto_now_add=True)
+    value = models.DecimalField()
+
+
+class ExerciseEvent(models.Model):
+    time = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(max_length=200)
+    duration = models.DurationField()
+
+
+class FoodEvent(models.Model):
+    time = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=250)
+    carb_count = models.DecimalField()
+    insulin_count = models.DecimalField()
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    # ratio is 1:insulin_to_carb_ratio
+    insulin_to_carb_ratio = models.IntegerField()
+    # ratio is 1:correction_factor
+    correction_factor = models.IntegerField()
+
