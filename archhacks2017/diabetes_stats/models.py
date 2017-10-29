7a9c147732e06cce4ad10344ad0fc0042eaa8dae
Madeline Silva <mad.silva.ic@gmail.com>
2017-10-28 19:45:36 -0500

    More work on API endpoints.

--- archhacks2017/diabetes_stats/models.py
+++ archhacks2017/diabetes_stats/models.py
@@ -1,5 +1,9 @@
+from datetime import timedelta
+from functools import reduce
+
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
 class UserProfile(models.Model):
@@ -9,24 +13,50 @@ class UserProfile(models.Model):
     # ratio is 1:correction_factor
     correction_factor = models.IntegerField()
 
+    def __str__(self):
+        return self.user.username
+
+    def number_of_points_logged(self):
+        blood = BloodGlucoseEvent.objects.all().count()
+        exercise = ExerciseEvent.objects.all().count()
+        food = FoodEvent.objects.all().count()
+        return blood + exercise + food
+
+    def week_average_glucose(self):
+        starttime = timezone.now() - timedelta(days=7)
+        endtime = timezone.now()
+        blood = BloodGlucoseEvent.objects.filter(time__range=[starttime, endtime]).all()
+        for b in blood:
+            sum += b.value
+        return sum/blood.count()
+
 
 class BloodGlucoseEvent(models.Model):
-    user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     time = models.DateTimeField(auto_now_add=True)
     value = models.DecimalField(decimal_places=1, max_digits=6)
 
+    def __str__(self):
+        return self.user_profile.user.username + " at " + str(self.time)
+
 
 class ExerciseEvent(models.Model):
-    user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     time = models.DateTimeField(auto_now_add=True)
     type = models.CharField(max_length=200)
     duration = models.DurationField()
 
+    def __str__(self):
+        return self.user_profile.user.username + " at " + str(self.time)
+
 
 class FoodEvent(models.Model):
-    user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     time = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=250)
     carb_count = models.DecimalField(decimal_places=1, max_digits=6)
     insulin_count = models.DecimalField(decimal_places=1, max_digits=6)
+
+    def __str__(self):
+        return self.user_profile.user.username + " at " + str(self.time)
 
