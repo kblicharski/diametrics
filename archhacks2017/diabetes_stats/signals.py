6d3bb2bb75daa2e2eb8e4e1912d79e3ef02afaac
Zain Khan <zkhan4123@gmail.com>
2017-10-28 22:59:29 -0500

    Equation to calculate insulin ratio

--- archhacks2017/diabetes_stats/signals.py
+++ archhacks2017/diabetes_stats/signals.py
@@ -1,3 +1,4 @@
+import numpy as np
 from django.dispatch import receiver
 from django.core.signals import post_save
 
@@ -9,12 +10,20 @@ from .models import BloodGlucoseEvent, ExerciseEvent, FoodEvent
 @receiver(post_save, sender=FoodEvent)
 def calculate_insulin_ratio(sender, instance, **kwargs):
     user_profile = instance.user_profile
-    blood_glucose = user_profile.bloodglucoseevent_set.all()
-    exercise = user_profile.exerciseevent_set.all()
-    food = user_profile.foodevent_set.all()
+    foods = user_profile.foodevent_set.all()
 
-    newval = 0
+    insulin_data=[]
+    carb_data=[]
+    weight=[]
+    for food in foods:
+        insulin_data.append(food.insulin_count)
+        carb_data.append(food.carb_count)
+        weight.append(food.time)
 
-    user_profile.insulin_to_carb_ratio = newval
+    ratios = [insulin/carb for insulin,carb in zip(insulin_data, carb_data)]
+
+    avg = np.average(ratios, axis=1, weights=weight)
+
+    user_profile.insulin_to_carb_ratio = avg
     user_profile.save()
 
